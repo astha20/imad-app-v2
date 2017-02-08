624df194d5f22abe304c3685800f2900ff7140fe
astha soni <asthasoni912@gmail.com>
2017-02-08 21:06:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,16 @@
 console.log('Loaded!');
+
+//change text of the main-text div
 var element = document.getElementById('main-text');
 element.innerHTML = 'new value';
+
+//move the image
+var element = document.getElementById('madi');
+var marginLeft = 0;
+function movrRight(){
+    marginLeft = marginLeft + 2;
+    img.style.marginLeft = marginLeft + 'px';
+}
+img.onclick = function() {
+    var interval = setInterval(moveRight, 50);
+}
