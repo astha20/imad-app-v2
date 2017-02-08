6b7bfb7705422b3c5f5eacc15b0420bccd9a2388
astha soni <asthasoni912@gmail.com>
2017-02-08 21:08:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ element.innerHTML = 'new value';
 var element = document.getElementById('madi');
 var marginLeft = 0;
 function movrRight(){
-    marginLeft = marginLeft + 2;
+    marginLeft = marginLeft + 5;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function() {
